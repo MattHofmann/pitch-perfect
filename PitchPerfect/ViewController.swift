37f735c1f525db7fd6ded4df4c219056d7175815
matthiashofmann <mhofm@me.com>
2016-08-09 13:06:39 +0900

    add Autolayout and Buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,15 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        print ("record button pressed")
+        recordingLabel.text = "Recording in progress"
+    }
+
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop recording button pressed")
+        recordingLabel.text = "Tap to Record"
+    }
+    
 }
 
